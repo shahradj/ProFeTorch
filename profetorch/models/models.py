ce2696c1bda7c06d675b089e7c4686ed5d20543b
Sachin Abeywardana <sachin.abeywardana@gmail.com>
2019-11-29 14:04:10 +1100

    moved lr_finder to utils

--- profetorch/models/models.py
+++ profetorch/models/models.py
@@ -44,50 +44,6 @@ class Model(nn.Module):
 
         self.model = learner.model
         
-    def find_appropriate_lr(self, df, lr_diff:int = 15, loss_threshold:float = .05, adjust_value:float = 1, plot:bool = False) -> float:
-        """
-        Automatic LR finder. Credit https://forums.fast.ai/t/automated-learning-rate-suggester/44199
-        """
-        #Run the Learning Rate Finder
-        model = self.create_learner(df)
-        model.lr_find()
-        
-        #Get loss values and their corresponding gradients, and get lr values
-        losses = np.array(model.recorder.losses)
-        assert(lr_diff < len(losses))
-        loss_grad = np.gradient(losses)
-        lrs = model.recorder.lrs
-        
-        #Search for index in gradients where loss is lowest before the loss spike
-        #Initialize right and left idx using the lr_diff as a spacing unit
-        #Set the local min lr as -1 to signify if threshold is too low
-        r_idx = -1
-        l_idx = r_idx - lr_diff
-        while (l_idx >= -len(losses)) and (abs(loss_grad[r_idx] - loss_grad[l_idx]) > loss_threshold):
-            local_min_lr = lrs[l_idx]
-            r_idx -= 1
-            l_idx -= 1
-
-        lr_to_use = local_min_lr * adjust_value
-        self.lr = lr_to_use
-        
-        if plot:
-            # plots the gradients of the losses in respect to the learning rate change
-            plt.plot(loss_grad)
-            plt.plot(len(losses)+l_idx, loss_grad[l_idx],markersize=10,marker='o',color='red')
-            plt.ylabel("Loss")
-            plt.xlabel("Index of LRs")
-            plt.show()
-
-            plt.plot(np.log10(lrs), losses)
-            plt.ylabel("Loss")
-            plt.xlabel("Log 10 Transform of Learning Rate")
-            loss_coord = np.interp(np.log10(lr_to_use), np.log10(lrs), losses)
-            plt.plot(np.log10(lr_to_use), loss_coord, markersize=10,marker='o',color='red')
-            plt.show()
-            
-        # return lr_to_use
-        
     def predict(self, df):
         x, _ = create_tensors(df.copy(), self.moments, predict=True)
         mean, sd = self.moments['y']
