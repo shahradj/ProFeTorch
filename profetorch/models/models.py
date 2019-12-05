c6f9401d386fc2d61bef02b81d3695f7553be8df
Sachin Abeywardana <sachin.abeywardana@gmail.com>
2019-12-05 14:56:43 +1100

    Changed how loss is printed to increase epochs.

--- profetorch/models/models.py
+++ profetorch/models/models.py
@@ -6,32 +6,19 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from fastai.basics import Learner, LearnerCallback
+from fastai.basics import Learner, LearnerCallback, Callback
 
 from .blocks import DefaultModel, DefaultQModel
 from ..data.data import create_db, create_tensors, get_moments
 from ..losses import q_loss
+from ..callbacks import *
 
 __all__ = ['Model']
-    
-class L1Loss(LearnerCallback):
-    def __init__(self, learn, beta=1e-2):
-        super().__init__(learn)
-        self.beta = beta
-    
-    def on_backward_end(self, **kwargss):
-        weights = {k:v for k,v in self.learn.model.named_parameters() 
-                   if not 'bias' in k}
-        
-        for k, v in weights.items():
-            sign = torch.ones_like(v)
-            sign[v<0] = -1
-            v = v - self.beta * sign
-            self.learn.model.state_dict()[k].copy_(v)
 
 
 class Model(nn.Module):
-    def __init__(self, df, model=None, model_args=None, quantiles=[0.05, 0.5, 0.95], loss=None, bs=128, lr=0.3, alpha=0, beta=0.1):
+    def __init__(self, df, model=None, model_args=None, quantiles=[0.05, 0.5, 0.95], 
+                 loss=None, bs=128, epochs=100, lr=0.3, alpha=0, beta=0.1, silent=True):
         """
         parameters:
         - df: dataset used in training dataset.
@@ -39,6 +26,7 @@ class Model(nn.Module):
         - loss (optional): loss function: Default l1 loss.
         - bs (optional): batchsize
         - alpha (optional): weight decay
+        - silent: whether to silence output or not
         """
         super().__init__()
         self.moments = get_moments(df.copy())
@@ -53,14 +41,16 @@ class Model(nn.Module):
         self.alpha = alpha
         self.beta = beta
         self.bs = bs
+        self.epochs = epochs
+        self.silent = silent
         
-    def fit(self, df=None, epochs=20):
+    def fit(self, df=None):
         # self.find_appropriate_lr(df)
         learner = self.create_learner(df)
-        cb = L1Loss(learner, self.beta)
+        cb = [L1Loss(learner, self.beta), PrintLoss(learner)]
         # breakpoint()
         
-        learner.fit_one_cycle(epochs, self.lr, callbacks=cb)
+        learner.fit_one_cycle(self.epochs, self.lr, callbacks=cb)
 
         self.model = learner.model
         
@@ -75,6 +65,7 @@ class Model(nn.Module):
     
     def create_learner(self, df):
         db = create_db(df, bs=self.bs, moments=self.moments)
-        learner = Learner(db, self.model, loss_func=self.loss, wd=self.alpha)
+        # breakpoint()
+        learner = Learner(db, self.model, loss_func=self.loss, wd=self.alpha, silent=True)
         return learner
         
